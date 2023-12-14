69e1ca881185a890ed3aad2099f71ab1d064f948
Anna Ballot <annaballot@gmail.com>
2023-12-14 20:04:07 +0000

    Tidy up of PHP file

--- get_avg_sound.php
+++ get_avg_sound.php
@@ -1,8 +1,5 @@
 <?php
 
-// if(isset($_GET["sound"])) {
-//    $sound = $_GET["sound"]; // get sound value from HTTP GET
-
    $servername = "localhost";
    $username = "Arduino";
    $password = "pass";
@@ -18,37 +15,9 @@
 
 // Perform query
 if ($result = $conn -> query("SELECT AVG(sound_value) as avgSoundVal FROM tbl_sound_readings")) {
-   // echo "Returned rows are: " . mysqli_num_rows($result);
-   // echo "Returned rows are: " . mysqli_fetch_all($result);
-
-   // Numeric array
-// $row = mysqli_fetch_array($result, MYSQLI_NUM);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-
-// printf ("%s (%s)\n", $row[0], $row[1]);
-// echo $row[0];
-
-
-// $arraytest = array(, "green", "blue", "yellow");
-
-// foreach ($row as $value) {
-// echo $value;
-// echo "\n";
-//  }
-
-
-
-  //create an array
-//   $emparray = array();
-//   while($rowTest =mysqli_fetch_assoc($result))
-//   {
-//       $emparray[] = $rowTest;
-//   }
-
   echo json_encode($row);
-
-
 
 
    // Free result set
@@ -56,7 +25,5 @@ $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
  }
 
    $conn->close();
-// } else {
-//    echo "sound is not set";
-// }
+
 ?>
